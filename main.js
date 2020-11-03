104efdfea442d1ea96123da5301209f298885869
Justin <justinmbaugess@gmail.com>
2020-11-03 17:11:36 -0500

    finished

--- main.js
+++ main.js
@@ -1,28 +1,48 @@
 // You should NOT change the HTML or CSS in this project (at least until you reach
 // the bonus objectives). Focus on the JavaScript.
 
-const findInput = document.querySelector(".find-input")
-const replaceInput = document.querySelector(".replace-input")
-const replaceAllButton = document.querySelector(".replace-all-button")
+const findInput = document.querySelector(".find-input");
+const replaceInput = document.querySelector(".replace-input");
+const replaceAllButton = document.querySelector(".replace-all-button");
 
 // The following variable holds your OUTER ARRAY of row elements.
 // Later you will need an OUTER LOOP to loop over the individual elements within
 // this array.
-const rowElements = document.querySelectorAll(".row")
+const rowElements = document.querySelectorAll(".row");
 
 // When you call the function belwo, it will get and return an INNER ARRAY
 // containing the cell elements for a given row.
 // Call this function from WITHIN your row elements loop. Then you will, in turn,
 // need to loop over the resulting cell elements. But where should this whole
 // NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
+
+
+
 function getCellElements (currentRowElement) {
-    return currentRowElement.querySelectorAll(".cell")
+    return currentRowElement.querySelectorAll(".cell");
 }
+    
+
 
 
 // YOUR CODE GOES HERE
+replaceAllButton.addEventListener("click", function(){
+    let find = findInput.value;
+    let replaceValue = replaceInput.value;
+    for(i = 0; i < rowElements.length; i++){
+        let cellElements = getCellElements(rowElements[i]);
+        // console.log(cellElements);
+        for(j = 0; j < cellElements.length; j++){
+            let elements = cellElements[j]
+            console.log(elements.innerHTML)
+            let n = elements.innerHTML.includes(find)
+            if(n){
+                elements.innerHTML = elements.innerHTML.replace(find, replaceValue);  
+            }    
+        }
+    }
 
-
+})
 // One last thing: dedicate very careful attention to using variables and
 // naming them accurately.
 // And when you change the value you are assigning to a variable, don't
